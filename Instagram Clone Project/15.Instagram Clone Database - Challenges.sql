fb02f89f86b3ed622eb5a750545f5a1915de6f47
Kishoran02 <jillakishoran2@gmail.com>
2025-04-09 13:10:24 +0530

    Update 15.Instagram Clone Database - Challenges.sql (#1)

--- Instagram Clone Project/15.Instagram Clone Database - Challenges.sql
+++ Instagram Clone Project/15.Instagram Clone Database - Challenges.sql
@@ -104,18 +104,16 @@ HAVING total_likes_by_user = (SELECT COUNT(*) FROM photos);
 
 /*We also have a problem with celebrities
 Find users who have never commented on a photo*/
-SELECT username,comment_text
-FROM users
-LEFT JOIN comments ON users.id = comments.user_id
-GROUP BY users.id
-HAVING comment_text IS NULL;
+select users.username 
+from users 
+left join comments on users.id = comments.user_id
+where comments.id is null;
 
-SELECT COUNT(*) FROM
-(SELECT username,comment_text
-	FROM users
-	LEFT JOIN comments ON users.id = comments.user_id
-	GROUP BY users.id
-	HAVING comment_text IS NULL) AS total_number_of_users_without_comments;
+select count(*) as `total_no_of_users` 
+from (select users.username 
+from users 
+left join comments on users.id = comments.user_id
+where comments.id is null) as total_number_of_users_without_comments;
 
 /*Mega Challenges
 Are we overrun with bots and celebrity accounts?
@@ -164,25 +162,20 @@ SELECT COUNT(*) FROM
 /*Are we overrun with bots and celebrity accounts?
 Find the percentage of our users who have either never commented on a photo or have commented on photos before*/
 
-SELECT tableA.total_A AS 'Number Of Users who never commented',
-		(tableA.total_A/(SELECT COUNT(*) FROM users))*100 AS '%',
-		tableB.total_B AS 'Number of Users who commented on photos',
-		(tableB.total_B/(SELECT COUNT(*) FROM users))*100 AS '%'
-FROM
-	(
-		SELECT COUNT(*) AS total_A FROM
-			(SELECT username,comment_text
-				FROM users
-				LEFT JOIN comments ON users.id = comments.user_id
-				GROUP BY users.id
-				HAVING comment_text IS NULL) AS total_number_of_users_without_comments
-	) AS tableA
-	JOIN
-	(
-		SELECT COUNT(*) AS total_B FROM
-			(SELECT username,comment_text
-				FROM users
-				LEFT JOIN comments ON users.id = comments.user_id
-				GROUP BY users.id
-				HAVING comment_text IS NOT NULL) AS total_number_users_with_comments
-	)AS tableB
+select tableA.table_A  as 'Number Of Users who never commented',
+            (tableA.table_A / (select count(*) from users))*100 as '%',
+            tableB.table_B as 'Number Of Users who commented on photos',
+            (tableB.table_B / (select count(*) from users))*100 as '%'
+from   (
+			select count(*) as table_A 
+            from (select users.username from users 
+            left join comments on users.id = comments.user_id
+            where comments.id is null) as total_number_of_users_without_comments
+		) as tableA
+        join 
+        (    
+			select count(distinct username) as table_B
+            from (select users.username,comments.comment_text from users 
+            left join comments on users.id = comments.user_id
+            where comments.id is not null) as total_number_of_users_comments
+		) as tableB;
